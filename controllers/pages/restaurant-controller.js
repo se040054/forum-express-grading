1795138ed0078e3d187a6c5f9ac66ee4203f733d
se040054 <peter8822967@gmail.com>
2023-08-29 19:32:16 +0800

    feat: add restaurant-services

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,47 +1,9 @@
 const { Restaurant, Category, User, Comment } = require('../../models')
-const { deletedCategoryFilter } = require('../../helpers/deleted-filter-helper')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+const restaurantServices = require('../../services/restaurant-services')
+
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const categoryId = Number(req.query.categoryId) || '' // 注意req.query是字串要轉型別，全部要給空字串
-    const DEFAULT_LIMIT = 9
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        raw: true,
-        nest: true,
-        include: Category,
-        where: {
-          ...categoryId ? { categoryId } : {}
-          // 要注意空物件永遠是true，這邊用成物件展開是為了擴充 實際上拿掉也可以跑
-        },
-        limit,
-        offset
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
-        const LikedRestaurantsId = req.user && req.user.LikedRestaurants.map(lr => lr.id)
-        // fr是簡寫 , 會返回一個只包含此使用者喜愛餐廳id的陣列
-        restaurants.rows = restaurants.rows.map(r => ({ // 小括號代替return,注意這裡不能改整個restaurants,要選rows
-          ...r,
-          description: r.description.substring(0, 50), // 雖然展開的時候也有屬性了，但後面的keyvalue可以覆蓋前面的keyvalue
-          isFavorited: favoritedRestaurantsId.includes(r.id),
-          isLiked: LikedRestaurantsId.includes(r.id)
-        })
-        )
-        categories = deletedCategoryFilter(categories)
-        return res.render('restaurants', {
-          restaurants: restaurants.rows, // 注意這裡要用rows
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
---
+++ services/restaurant-services.js
@@ -0,0 +1,50 @@
+const { Restaurant, Category } = require('../models')
+const { deletedCategoryFilter } = require('../helpers/deleted-filter-helper')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+
+const restaurantServices = {
+  getRestaurants: (req, callback) => { // 抽換成服務的時候callback資料回去
+    const categoryId = Number(req.query.categoryId) || '' // 注意req.query是字串要轉型別，全部要給空字串
+    const DEFAULT_LIMIT = 9
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        raw: true,
+        nest: true,
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+          // 要注意空物件永遠是true，這邊用成物件展開是為了擴充 實際上拿掉也可以跑
+        },
+        limit,
+        offset
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = // fr是簡寫 , 會返回一個只包含此使用者喜愛餐廳id的陣列
+          req.user?.FavoritedRestaurants ? req.user.FavoritedRestaurants.map(fr => fr.id) : []
+        const LikedRestaurantsId =
+          req.user?.LikedRestaurants ? req.user.LikedRestaurants.map(lr => lr.id) : []
+
+        restaurants.rows = restaurants.rows.map(r => ({ // 小括號代替return,注意這裡不能改整個restaurants,要選rows
+          ...r,
+          description: r.description.substring(0, 50), // 雖然展開的時候也有屬性了，但後面的keyvalue可以覆蓋前面的keyvalue
+          isFavorited: favoritedRestaurantsId.includes(r.id),
+          isLiked: LikedRestaurantsId.includes(r.id)
+        })
+        )
+        categories = deletedCategoryFilter(categories)
+        return callback(null, {
+          restaurants: restaurants.rows, // 注意這裡要用rows
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
+      })
+      .catch(err => callback(err))
+  }
+}
+module.exports = restaurantServices

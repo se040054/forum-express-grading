3191b06f93a0339872f7e58735572ef20fb183b7
se040054 <peter8822967@gmail.com>
2023-08-31 06:11:11 +0800

    feat: modify api unauthorize response

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,6 +1,16 @@
 const passport = require('../config/passport')
 
-const authenticated = passport.authenticate('jwt', { session: false })
+// 原先寫法 無法客製錯誤訊息
+// const authenticated = passport.authenticate('jwt', { session: false })
+// 其實 authenticate第三參數可以接受cb函式 但是要自己處理驗證成功時 req.user 給資料
+
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => { // (err, user) 是cb函式
+    if (err || !user) return res.status(401).json({ status: 'error', message: '未授權' })
+    req.user = user
+    next()
+  })(req, res, next)
+}
 
 const authenticatedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) return next()

12e38b130a37345001fcc9afe17244ac0a9ac127
se040054 <peter8822967@gmail.com>
2023-09-01 05:57:08 +0800

    render : category page

--- controllers/pages/category-controller.js
+++ controllers/pages/category-controller.js
@@ -1,59 +1,32 @@
-const { Category, Restaurant } = require('../../models')
-const { deletedCategoryFilter, deletedCategoryId } = require('../../helpers/deleted-filter-helper')
+const categoryServices = require('../../services/category-services')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Promise.all([
-      Category.findAll({ raw: true }),
-      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
-    ])
-      .then(([categories, category]) => {
-        categories = deletedCategoryFilter(categories)
-        res.render('admin/categories', { categories, category })
-      })
-      .catch(err => next(err))
+    categoryServices.getCategories(req, (err, data) => err ? next(err) : res.render('admin/categories', data))
   },
   postCategory: (req, res, next) => {
-    const { name } = req.body
-    if (!name) throw new Error('類別名稱不可為空')
-    return Category.create({ name })
-      .then(() => {
-        req.flash('success_messages', '新增分類成功')
-        res.redirect('/admin/categories')
-      })
-      .catch(err => next(err))
+    categoryServices.postCategory(req, (err, data) => {
+      if (err) return next(err)
+      req.flash('success_messages', '分類新增成功')
+      req.session.nweCategory = data // 保留新增資料備用做法
+      return res.redirect('/admin/categories')
+    })
   },
   putCategories: (req, res, next) => {
-    const { name } = req.body
-    if (!name) throw new Error('類別名稱不可為空')
-    return Category.findByPk(req.params.id)
-      .then(category => {
-        if (!category) throw new Error('此類別不存在')
-        return category.update({ name })
-      })
-      .then(() => {
-        req.flash('success_messages', '修改分類成功')
-        res.redirect('/admin/categories')
-      })
-      .catch(err => next(err))
+    categoryServices.putCategories(req, (err, data) => {
+      if (err) return next(err)
+      req.flash('success_messages', '分類編輯成功')
+      req.session.editedCategory = data // 保留新增資料備用做法
+      return res.redirect('/admin/categories')
+    })
   },
   deleteCategory: (req, res, next) => {
-    return Promise.all([
-      Category.findByPk(req.params.id),
-      Restaurant.findAll({ where: { categoryId: req.params.id } })
-    ])
-      .then(async ([category, restaurants]) => {
-        if (!category) throw new Error('此類別不存在')
-        await Promise.all(restaurants.map(restaurant => { // 注意這裡用map才能取得返回值
-          return restaurant.update({ categoryId: String(deletedCategoryId) })
-        }))
-        return await category.destroy()
-      })
-      .then(() => {
-        req.flash('success_messages', '刪除類別成功')
-        res.redirect('/admin/categories')
-      })
-      .catch(err => next(err))
+    categoryServices.deleteCategory(req, (err, data) => {
+      if (err) return next(err)
+      req.flash('success_messages', '分類刪除成功')
+      req.session.deletedCategory = data // 保留新增資料備用做法
+      return res.redirect('/admin/categories')
+    })
   }
 }
 
